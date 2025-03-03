463554c03dbf2af3cab9834453a80f07dc3092fd
Syntrice <syntrice@protonmail.com>
2025-03-03 13:38:37 +0000

    feat: Create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <header className="p-5 bg-linear-65 gradient text-text-400">
+            <h1 className="text-2xl font-bold">Meme Generator</h1>
+        </header>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App.tsx'
+import Main from './components/Main.tsx'
+import Header from './components/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header/>
+    <Main />
   </StrictMode>,
 )
