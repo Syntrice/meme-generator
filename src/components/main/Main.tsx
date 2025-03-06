43352e13294ee5a86802b99683dfd320bdc44eb0
syntrice <syntrice@protonmail.com>
2025-03-06 14:25:32 +0000

    feat: Update meme text from MemeInput
    Using callbacks and onChange event handler, update the meme display
    to use the text from the meme input boxes.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,22 +1,38 @@
 import { MemeData } from "../../scripts/types";
-import MemeDisplay from "./meme-display/MemeDisplay"
-import MemeInput from "./meme-input/MemeInput"
-import React, { useState } from "react"
+import MemeDisplay from "./meme-display/MemeDisplay";
+import MemeInput from "./meme-input/MemeInput";
+import React, { useState } from "react";
 
 export default () => {
   const [memeData, setMemeData] = useState<MemeData>({
     imageUrl: "http://i.imgflip.com/1bij.jpg",
     topText: "ONE DOES NOT SIMPLY",
-    bottomText: "WALK INTO MORDOR"
-    });
+    bottomText: "WALK INTO MORDOR",
+  });
 
+  function handleTopTextChanged(text: string) {
+    setMemeData(prev => {return {...prev, topText: text}})
+  }
+
+  function handleBottomTextChanged(text: string) {
+    setMemeData(prev => {return {...prev, bottomText: text}})
+
+  }
+
+  function handleNewImageRequested() {
+    console.log("New image requested")
+  }
 
   return (
     <main className="p-5">
       <div className="wrapper">
-        <MemeInput/>
-        <MemeDisplay data={memeData}/>
+        <MemeInput
+          onTopTextChanged={handleTopTextChanged}
+          onBottomTextChanged={handleBottomTextChanged}
+          onNewImageRequested={handleNewImageRequested}
+        />
+        <MemeDisplay data={memeData} />
       </div>
     </main>
-  )
-}
+  );
+};
--- src/components/main/meme-input/MemeInput.tsx
+++ src/components/main/meme-input/MemeInput.tsx
@@ -1,17 +1,46 @@
-export default () => {
-    return (
-        <div className="flex flex-col gap-10">
-            <div className="flex flex-wrap justify-between gap-5 text-light-100">
-                <div className="grow min-w-2xs">
-                    <label htmlFor="top-text">Top text</label>
-                    <input className="text-input" type="text" id="top-text" name="topText" placeholder="Shut up" />
-                </div>
-                <div className="grow min-w-2xs">
-                    <label htmlFor="bottom-text">Bottom text</label>
-                    <input className="text-input" type="text" id="bottom-text" name="bottomText" placeholder="And take my money" />
-                </div>
-            </div>
-            <button className="block w-full p-5 rounded-md gradient hover:gradient-dark text-light-400"><b>Get a new meme image 🖼️</b></button>
-        </div>
-    )
+interface MemeInputProps {
+  onTopTextChanged: (text: string) => void;
+  onBottomTextChanged: (text: string) => void;
+  onNewImageRequested: () => void;
 }
+
+export default ({
+  onTopTextChanged,
+  onBottomTextChanged,
+  onNewImageRequested,
+}: MemeInputProps) => {
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-wrap justify-between gap-5 text-light-100">
+        <div className="grow min-w-2xs">
+          <label htmlFor="top-text">Top text</label>
+          <input
+            className="text-input"
+            type="text"
+            id="top-text"
+            name="topText"
+            placeholder="ONE DOES NOT SIMPLY"
+            onChange={(e) => onTopTextChanged(e.currentTarget.value)}
+          />
+        </div>
+        <div className="grow min-w-2xs">
+          <label htmlFor="bottom-text">Bottom text</label>
+          <input
+            className="text-input"
+            type="text"
+            id="bottom-text"
+            name="bottomText"
+            placeholder="WALK INTO MORDOR"
+            onChange={(e) => onBottomTextChanged(e.currentTarget.value)}
+          />
+        </div>
+      </div>
+      <button
+        className="block w-full p-5 rounded-md gradient hover:gradient-dark text-light-400"
+        onClick={onNewImageRequested}
+      >
+        <b>Get a new meme image 🖼️</b>
+      </button>
+    </div>
+  );
+};
